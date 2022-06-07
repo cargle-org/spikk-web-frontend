1167c77cde79550e15782ae994702975831fe8e8
JosephAdewole <josephadewole1@gmail.com>
2022-06-07 21:11:57 +0100

    added logo and background to Landing header component

--- src/components/landing/LandingHeader.tsx
+++ src/components/landing/LandingHeader.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { Image, Box, Button } from '@chakra-ui/react'
+import spikklogo from "./spikklogo.png";
 const LandingHeader = () => {
   return (
-    <div>LandingHeader</div>
+    <div>
+      <Box bg='#0D0D26'>
+        <Image p={6} src={spikklogo} alt='Spikk Logo' />
+        <div style={{color:'white'}}>
+          <div>Home</div>
+        </div>
+        <div style={{color:'white'}}>
+          <div>About</div>
+        </div>
+      </Box>
+
+      <Box bg='#0D0D26'>
+        <Box>
+          <Button bg='#FBA819' color='#0D0D26'>Join</Button>
+        </Box>
+      </Box>
+    </div>
   )
 }
 
--- src/pages/Landing/Hompage.tsx
+++ src/pages/Landing/Hompage.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import LandingFooter from '../../components/landing/LandingFooter'
+import LandingHeader from '../../components/landing/LandingHeader'
 
 const Hompage = () => {
   return (
-    <div>Hompage</div>
+    <div>
+      <LandingHeader/>
+      <div>Home Body</div>
+      <LandingFooter/>
+    </div>
   )
 }
 
