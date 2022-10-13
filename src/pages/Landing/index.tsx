821f692b8fede8c7219c984a8915e910fd3cd9de
Olaolu Johnson <johnsonolaolu@gmail.com>
2022-10-13 11:35:24 +0100

    updated

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -6,20 +6,13 @@ import PickerSelected from "../Dashboard/BuyAnything/PickerSelected";
 import OrderHome from "../Orders/OrderHome";
 const Homepage = React.lazy(() => import("./Homepage"));
 const PrivacyPolicy = React.lazy(() => import("./PrivacyPolicy"));
-// const About = React.lazy(() => import('../About/About'))
-// const BuyAnything = React.lazy(() => import('../Dashboard/BuyAnything/BuyAnything'))
-// const BuyAnythingLocation = React.lazy(() => import('../Dashboard/BuyAnything/BuyAnythingLocation'))
-// const BuyAnythingOrderSummary = React.lazy(() => import('../Dashboard/BuyAnything/BuyAnythingOrderSummary'))
-// const AvailablePickers = React.lazy(() => import('../Dashboard/BuyAnything/AvailablePickers'))
-// const FindingPickers = React.lazy(() => import('../Dashboard/BuyAnything/FindingPickers'))
-// const Dashboard = React.lazy(() => import('../Dashboard/Dashboard'))
 
 const LandingRoutes = () => {
   return (
     <>
       <LandingHeader />
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
       <LandingFooter />
